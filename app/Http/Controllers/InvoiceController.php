e75e69e69d7ca409cc975fae469102dfc853d32e
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-11 17:00:22 +0700

    Create Invoice Controller User

---
+++ app/Http/Controllers/InvoiceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invoice;
+use Illuminate\Http\Request;
+
+class InvoiceController extends Controller
+{
+    public function index(Request $request)
+
+    {
+        return Invoice::query()
+            ->select('id', 'code', 'amount', 'note', 'created_at')
+            ->whereBelongsTo($request->user())
+            ->latest()
+            ->get()
+            ->map(fn ($invoice) => [
+                'id' => $invoice->id,
+                'code' => $invoice->code,
+                'note' => $invoice->note,
+                'created_at' => $invoice->created_at->format('d M Y H:i'),
+            ]);
+    }
+}

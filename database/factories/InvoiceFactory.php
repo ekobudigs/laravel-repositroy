15ac00ee91bf05fa266ccfed4d946128c2f8914d
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-11 16:18:48 +0700

    create factory invoice

---
+++ database/factories/InvoiceFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
+ */
+class InvoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'code' => 'INV-' . $this->faker->randomNumber(),
+            'amount' =>  $this->faker->randomNumber(),
+            'note' =>  $this->faker->sentence(),
+        ];
+    }
+}

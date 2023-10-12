4a5f5ababde63d3abc4c62a122bd144bab57a4a5
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-12 10:57:43 +0700

    Update Invoice Repository use resource

--- app/Repositories/InvoiceRepository.php
+++ app/Repositories/InvoiceRepository.php
@@ -4,22 +4,19 @@ namespace App\Repositories;
 
 use App\Models\User;
 use App\Models\Invoice;
+use App\Http\Resources\InvoiceUserResource;
+use App\Repositories\Intervace\InvoiceRepositoryIntervace;
 
-class InvoiceRepository
+class InvoiceRepository implements InvoiceRepositoryIntervace
 {
     public function byUser(User $user)
     {
-        return Invoice::query()
+        $invoice = Invoice::query()
             ->select('id', 'code', 'amount', 'note', 'created_at')
             ->whereBelongsTo($user)
             ->latest()
-            ->get()
-            ->map(fn ($invoice) => [
-                'id' => $invoice->id,
-                'code' => $invoice->code,
-                'note' => $invoice->note,
-                'amount' => number_format($invoice->amount, 0, '.', '.'),
-                'created_at' => $invoice->created_at->format('d M Y H:i'),
-            ]);
+            ->paginate(10);
+
+        return InvoiceUserResource::collection($invoice);
     }
 }

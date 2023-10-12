3f7c1ec58cc6ab3027291db514ef3af14f5c790c
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-12 10:02:59 +0700

    Repository Invoice

--- app/Http/Controllers/AdminPanel/InvoiceController.php
+++ app/Http/Controllers/AdminPanel/InvoiceController.php
@@ -5,19 +5,22 @@ namespace App\Http\Controllers\AdminPanel;
 use App\Models\Invoice;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class InvoiceController extends Controller
 {
-    public function index()
+    public function index(User $user)
     {
         return Invoice::query()
             ->select('id', 'code', 'amount', 'note', 'created_at')
             ->latest()
+            ->whereBelongsTo($user)
             ->get()
             ->map(fn ($invoice) => [
                 'id' => $invoice->id,
                 'code' => $invoice->code,
                 'note' => $invoice->note,
+                'amount' => number_format($invoice->amount, 0, '.', '.'),
                 'created_at' => $invoice->created_at->format('d M Y H:i'),
             ]);
     }

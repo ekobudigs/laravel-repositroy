650d93b4db7118fa6d1f59ca2a2302f941a00d09
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-12 11:02:00 +0700

    update invoice controller

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -5,22 +5,33 @@ namespace App\Http\Controllers;
 use App\Models\Invoice;
 use Illuminate\Http\Request;
 
+use App\Repositories\InvoiceRepository;
+use App\Repositories\Intervace\InvoiceRepositoryIntervace;
+
 class InvoiceController extends Controller
 {
+
+    public function __construct(protected InvoiceRepositoryIntervace $invoiceRepository)
+    {
+    }
     public function index(Request $request)
 
     {
-        return Invoice::query()
-            ->select('id', 'code', 'amount', 'note', 'created_at')
-            ->whereBelongsTo($request->user())
-            ->latest()
-            ->get()
-            ->map(fn ($invoice) => [
-                'id' => $invoice->id,
-                'code' => $invoice->code,
-                'note' => $invoice->note,
-                'amount' => number_format($invoice->amount, 0, '.', '.'),
-                'created_at' => $invoice->created_at->format('d M Y H:i'),
-            ]);
+
+        return $this->invoiceRepository->byUser($request->user());
+
+
+        // return Invoice::query()
+        //     ->select('id', 'code', 'amount', 'note', 'created_at')
+        //     ->whereBelongsTo($request->user())
+        //     ->latest()
+        //     ->get()
+        //     ->map(fn ($invoice) => [
+        //         'id' => $invoice->id,
+        //         'code' => $invoice->code,
+        //         'note' => $invoice->note,
+        //         'amount' => number_format($invoice->amount, 0, '.', '.'),
+        //         'created_at' => $invoice->created_at->format('d M Y H:i'),
+        //     ]);
     }
 }
